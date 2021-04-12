e7a84430892b576700cfd7fd67d631735a490119
Johannes Schickling <schickling.j@gmail.com>
2021-04-12 13:55:47 +0200

    fix next build

--- packages/playground-azimuth/next.config.js
+++ packages/playground-azimuth/next.config.js
@@ -1,15 +1,3 @@
 module.exports = {
   future: { webpack5: true },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-    // currently needed because of this error
-    /*
-    ./sourcebit/schema/documents/blog.ts:1:32
-Type error: Cannot find module 'sourcebit/source-local' or its corresponding type declarations.
-    */
-  },
 }
