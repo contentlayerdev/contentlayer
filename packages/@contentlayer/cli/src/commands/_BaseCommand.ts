ff7e9beef459d50be58f62d1d2c3a5670fb1584e
Johannes Schickling <schickling.j@gmail.com>
2021-11-04 10:37:43 +0000

    cleanup based command

--- packages/@contentlayer/cli/src/commands/_BaseCommand.ts
+++ packages/@contentlayer/cli/src/commands/_BaseCommand.ts
@@ -24,10 +24,7 @@ export abstract class BaseCommand extends Command {
 
   execute = () =>
     pipe(
-      pipe(
-        clearCacheIfNeeded(this.clearCache),
-        T.chain(() => this.executeSafe),
-      ),
+      pipe(clearCacheIfNeeded(this.clearCache), T.zipRight(this.executeSafe)),
       core.runMain({
         tracingServiceName: 'contentlayer-cli',
         verbose: this.verbose || process.env.CL_DEBUG !== undefined,
@@ -42,39 +39,3 @@ const clearCacheIfNeeded = (shouldClearCache: boolean) =>
       yield* $(T.log('Cache cleared successfully'))
     }
   })
-
-// const runMain = (self: BaseCommand): T.Effect<T.DefaultEnv, never, void> =>
-//   T.gen(function* ($) {
-
-//     if (process.platform === 'win32') {
-//       yield* $(T.log('Warning: Contentlayer might not work as expected on Windows'))
-//     }
-
-//     const result = yield* $(pipe(self.executeSafe, T.provideSomeLayer(JaegerNodeTracing('contentlayer-cli')), T.result))
-
-//     if (result._tag === 'Failure') {
-//       const failOrCause = Cause.failureOrCause(result.cause)
-//       const errorWasManaged = failOrCause._tag === 'Left'
-
-//       if (!errorWasManaged) {
-//         yield* $(
-//           T.log(`\
-// This error shouldn't have happened. Please consider opening a GitHub issue with the stack trace below here:
-// https://github.com/contentlayerdev/contentlayer/issues`),
-//         )
-//       }
-
-//       // If failure was a managed error and no `--verbose` flag was provided, print the error message
-//       if (errorWasManaged && !self.verbose) {
-//         if (!core.isSourceFetchDataError(failOrCause.left) || !failOrCause.left.alreadyHandled) {
-//           yield* $(T.log(failOrCause.left))
-//         }
-//       }
-//       // otherwise for unmanaged errors or with `--verbose` flag provided, print the entire stack trace
-//       else {
-//         yield* $(T.log(pretty(result.cause)))
-//       }
-
-//       yield* $(T.succeedWith(() => process.exit(1)))
-//     }
-//   })
