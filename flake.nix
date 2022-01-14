cf9450dfa794c91c5a5159e05b8d3e02d9103ec3
Johannes Schickling <schickling.j@gmail.com>
2022-01-14 14:37:17 +0100

    nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+
+        devShell = with pkgs; pkgs.mkShell {
+          buildInputs = [
+            nodejs-16_x
+          ];
+        };
+
+      });
+}
