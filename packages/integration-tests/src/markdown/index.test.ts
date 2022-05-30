f6655d540640c4875ace4a2f320ae7da20528c50
Stefan Probst <stefanprobst@posteo.de>
2022-05-30 07:56:43 +0200

    test: add test for passing vfile.data from unified processor

--- packages/integration-tests/src/markdown/index.test.ts
+++ packages/integration-tests/src/markdown/index.test.ts
@@ -8,6 +8,7 @@ import rehypeStringify from 'rehype-stringify'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkParse from 'remark-parse'
 import remark2rehype from 'remark-rehype'
+import type { Plugin } from 'unified'
 import { expect, test, vi } from 'vitest'
 
 test('markdown builder pattern', async () => {
@@ -44,4 +45,44 @@ test('markdown builder pattern', async () => {
   )
 
   expect(spyFn).toHaveBeenCalledOnce()
+})
+
+test('pass vfile.data from markdown processor', async () => {
+  const Post = defineDocumentType(() => ({
+    name: 'Post',
+    filePathPattern: '**/*.md',
+    fields: {},
+  }))
+
+  const testDirPath = fileURLToPath(new URL('.', import.meta.url))
+
+  await fs.rm(path.join(testDirPath, '.contentlayer'), { recursive: true, force: true })
+
+  process.env['INIT_CWD'] = testDirPath
+
+  const testMessage = 'Hello, world!'
+
+  const testPlugin: Plugin = () => {
+    return function transformer(tree, file) {
+      file.data.test = testMessage
+    }
+  }
+
+  const source = await makeSource({
+    contentDirPath: path.join(testDirPath, 'posts'),
+    documentTypes: [Post],
+    markdown: {
+      remarkPlugins: [testPlugin],
+    },
+  })
+
+  await core.runMain({ tracingServiceName: 'test', verbose: false })(
+    core.generateDotpkg({ config: { source, esbuildHash: 'STATIC_HASH' }, verbose: true }),
+  )
+
+  const generated = JSON.parse(
+    await fs.readFile(path.join(testDirPath, '.contentlayer', 'generated/Post/post-1.md.json'), { encoding: 'utf-8' }),
+  )
+
+  expect(generated.body.data.test).toBe(testMessage)
 })
