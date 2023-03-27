9ac8edad697633cd779c3f753b7a3d69e28d9d9e
Johannes Schickling <schickling.j@gmail.com>
2023-03-27 09:16:25 +0200

    Upgrade deps

--- packages/integration-tests/src/image-field/index.test.ts
+++ packages/integration-tests/src/image-field/index.test.ts
@@ -44,7 +44,7 @@ test('mdx-image-field ', async () => {
 
   expect(allPosts[0].coverImage).toMatchInlineSnapshot(`
     {
-      "blurhashDataUrl": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAMAAADz0U65AAAACVBMVEV8Oe1uNNJ3OOKPJhaEAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAHUlEQVR4nGNgwAIYGRkhDCZGJqgAE0SIiQmEsAAAAyMAF5ferjEAAAAASUVORK5CYII=",
+      "blurhashDataUrl": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAMAAADz0U65AAAACVBMVEV8Ou5vNNN3OeJauo57AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAHUlEQVR4nGNgwAIYGRkhDCZGJqgAE0SIiQmEsAAAAyMAF5ferjEAAAAASUVORK5CYII=",
       "filePath": "posts/image-a.png",
       "format": "png",
       "height": 480,
