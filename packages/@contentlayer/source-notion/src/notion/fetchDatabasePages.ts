7663f0a096771c5389eafbe47065cad4a232cfc6
Martin PAUCOT <m.paucot@syneki.com>
2023-03-31 04:29:17 +0200

    fix(notion): remove debug console logs

--- packages/@contentlayer/source-notion/src/notion/fetchDatabasePages.ts
+++ packages/@contentlayer/source-notion/src/notion/fetchDatabasePages.ts
@@ -15,10 +15,8 @@ export const fetchDatabasePages = ({ databaseTypeDef }: FetchDatabasePagesArgs) 
     S.chain((client) =>
       S.async<unknown, unknown, PageObjectResponse[]>(async (emit) => {
         let nextCursor: string | undefined = undefined
-        console.log(`Querying pages for ${databaseTypeDef.name}`)
 
         do {
-          console.log(`Querying pages for ${databaseTypeDef.name} with cursor ${nextCursor}`)
           const res = await client.databases.query({
             database_id: databaseTypeDef.databaseId,
             start_cursor: nextCursor ?? undefined,
@@ -26,7 +24,6 @@ export const fetchDatabasePages = ({ databaseTypeDef }: FetchDatabasePagesArgs) 
             sorts: databaseTypeDef.query?.sorts,
           })
 
-          console.log(`Found ${res.results.length} pages`)
           nextCursor = res.next_cursor as string | undefined // NOTE: Throw type error and make res any if not typed, why???
           emit.single(res.results as PageObjectResponse[])
         } while (nextCursor)
--- packages/@contentlayer/source-notion/src/notion/fetchNotion.ts
+++ packages/@contentlayer/source-notion/src/notion/fetchNotion.ts
@@ -5,18 +5,9 @@ const queue = new PQueue({ interval: 1000, intervalCap: 4 })
 
 export const fetchNotion = (...args: Parameters<typeof fetch>) => {
   return queue.add(() =>
-    pRetry(
-      () => {
-        console.log(`Fetching ${args[0]}`)
-        return fetch(...args)
-      },
-      {
-        retries: 5,
-        onFailedAttempt: async (r) => {
-          console.warn(`WARN: Error while querying Notion, retry #${r.attemptNumber}`)
-          await new Promise((res) => setTimeout(res, 1000))
-        },
-      },
-    ),
+    pRetry(() => fetch(...args), {
+      retries: 5,
+      onFailedAttempt: async () => new Promise((res) => setTimeout(res, 1000)),
+    }),
   ) as Promise<Response>
 }
