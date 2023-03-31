510741c5d7012db0d8574fe37b4a967c60cdd8f1
Martin PAUCOT <m.paucot@syneki.com>
2023-03-31 18:17:43 +0200

    fix(notion): throw AbortError to fix p-retry

--- packages/@contentlayer/source-notion/src/notion/fetchNotion.ts
+++ packages/@contentlayer/source-notion/src/notion/fetchNotion.ts
@@ -1,13 +1,24 @@
-import PQueue from 'p-queue'
+import PQueue, { AbortError } from 'p-queue'
 import pRetry from 'p-retry'
 
 const queue = new PQueue({ interval: 1000, intervalCap: 4 })
 
 export const fetchNotion = (...args: Parameters<typeof fetch>) => {
   return queue.add(() =>
-    pRetry(() => fetch(...args), {
-      retries: 5,
-      onFailedAttempt: async () => new Promise((res) => setTimeout(res, 1000)),
-    }),
+    pRetry(
+      async () => {
+        const response = await fetch(...args)
+
+        if (!response.ok) {
+          throw new AbortError(response.statusText)
+        }
+
+        return response
+      },
+      {
+        retries: 5,
+        onFailedAttempt: async () => new Promise((res) => setTimeout(res, 1000)),
+      },
+    ),
   ) as Promise<Response>
 }
